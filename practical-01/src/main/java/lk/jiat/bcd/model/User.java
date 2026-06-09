ad10ddf010bd043464f040890e0111f79216442e
mvinduwara <mvinduwara@gmail.com>
2026-06-09 20:38:19 +0530

    Update User.java

--- practical-01/src/main/java/lk/jiat/bcd/model/User.java
+++ practical-01/src/main/java/lk/jiat/bcd/model/User.java
@@ -4,8 +4,33 @@ public class User {
 
     private String name;
     private String mobile;
+    private String email;
+    private String password;
 
     public User(){}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String name, String mobile, String email, String password) {
+        this.name = name;
+        this.mobile = mobile;
+        this.email = email;
+        this.password = password;
+    }
 
     public String getName() {
         return name;
