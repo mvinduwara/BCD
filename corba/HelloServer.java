60a0a7bfcfb89ca7420be64ff88cf1abcd5582c4
mvinduwara <mvinduwara@gmail.com>
2026-06-12 20:13:11 +0530

    Add CORBA server initialization to HelloServer
    Initialize and run a CORBA server in HelloServer: ORB.init, activate RootPOA, create a HelloWorldImple servant, convert it to a CORBA reference and narrow to Hello, resolve the NameService and bind the servant under the name "Hello", print a ready message and start the ORB. Exceptions are caught and rethrown as a RuntimeException.

--- corba/HelloServer.java
+++ corba/HelloServer.java
@@ -1,5 +1,37 @@
+import org.omg.CORBA.ORB;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NameComponent;
+import TestApp.Hello;
+import TestApp.HelloHelper;
+
 public class HelloServer {
     public static void main(String[] args) {
 
+        ORB orb = ORB.init(args, null);
+        try {
+            POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            rootpoa.the_POAManager().activate();
+
+            HelloWorldImple hello = new HelloWorldImple();
+
+            org.omg.CORBA.Object obj = rootpoa.servant_to_reference(hello);
+
+            Hello href = HelloHelper.narrow(obj);
+
+            org.omg.CORBA.Object obj2 = orb.resolve_initial_references("NameService");
+
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(obj2);
+
+            NameComponent path[] = ncRef.to_name("Hello");
+
+            ncRef.rebind(path, href);
+            System.out.println("CORBA Server ready");
+            orb.run();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
