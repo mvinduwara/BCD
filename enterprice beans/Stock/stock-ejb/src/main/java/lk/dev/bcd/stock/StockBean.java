1c383d75405e8c0068dda5a825013e48cf1fa675
mvinduwara <mvinduwara@gmail.com>
2026-06-17 20:22:09 +0530

    Update StockBean.java

--- enterprice beans/Stock/stock-ejb/src/main/java/lk/dev/bcd/stock/StockBean.java
+++ enterprice beans/Stock/stock-ejb/src/main/java/lk/dev/bcd/stock/StockBean.java
@@ -1,17 +1,19 @@
 package lk.dev.bcd.stock;
 
+import jakarta.ejb.Stateless;
 import java.util.HashMap;
 import java.util.Map;
 
+@Stateless
 public class StockBean implements StockService {
 
     private Map<String, Integer> stock = new HashMap<>();
 
     public StockBean() {
-        stock.put("Apple", 100);
-        stock.put("Pine Apple", 100);
-        stock.put("Orange", 100);
-        stock.put("Mango", 100);
+        stock.put("Apple", 52);
+        stock.put("Pine Apple", 34);
+        stock.put("Orange", 25);
+        stock.put("Mango", 77);
         stock.put("Peach", 100);
     }
 
