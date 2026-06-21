37e40d99076461684e023fe425742cf3b8164e1a
mvinduwara <mvinduwara@gmail.com>
2026-06-21 10:44:59 +0530

    Update AppSettingSessionBean.java

--- enterprice beans/ecomm-cdi/src/main/java/lk/dev/bcd/ejb/AppSettingSessionBean.java
+++ enterprice beans/ecomm-cdi/src/main/java/lk/dev/bcd/ejb/AppSettingSessionBean.java
@@ -3,7 +3,10 @@ package lk.dev.bcd.ejb;
 import jakarta.annotation.PostConstruct;
 import jakarta.ejb.Singleton;
 import jakarta.inject.Inject;
+import lk.dev.bcd.cdi.EmailNotifier;
 import lk.dev.bcd.cdi.MyService;
+import lk.dev.bcd.cdi.NotificationService;
+import lk.dev.bcd.cdi.SMSNotifier;
 import lk.dev.bcd.ejb.remote.AppSetting;
 
 @Singleton
@@ -15,6 +18,15 @@ public class AppSettingSessionBean implements AppSetting {
     public void init() {
         myService = new MyService();
     }
+
+//    @Inject
+//    private EmailNotifier emailNotifier;
+//
+//    @Inject
+//    private SMSNotifier smsNotifier;
+
+    @Inject
+    private NotificationService notificationService;
 
     @Override
     public String getName() {
