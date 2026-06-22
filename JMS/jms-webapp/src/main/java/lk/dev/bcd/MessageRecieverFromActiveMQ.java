68aca77a416fa5ea372a40b898cd929fc075850e
mvinduwara <mvinduwara@gmail.com>
2026-06-22 20:51:31 +0530

    Create MessageRecieverFromActiveMQ.java

---
+++ JMS/jms-webapp/src/main/java/lk/dev/bcd/MessageRecieverFromActiveMQ.java
@@ -0,0 +1,24 @@
+package lk.dev.bcd;
+
+import jakarta.ejb.ActivationConfigProperty;
+import jakarta.ejb.MessageDriven;
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
+
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType",propertyValue = "jakartha.jms.Topic"),
+        @ActivationConfigProperty(propertyName = "destinationLookup",propertyValue = "activeMQTopic"),
+        @ActivationConfigProperty(propertyName = "destination",propertyValue = "activeMQTopic"),
+        @ActivationConfigProperty(propertyName = "resourceAdapter   ",propertyValue = "activemq-rar-6.2.6")
+})
+public class MessageRecieverFromActiveMQ implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+        try {
+            System.out.println("Message Recieved:" + message.getBody(String.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
