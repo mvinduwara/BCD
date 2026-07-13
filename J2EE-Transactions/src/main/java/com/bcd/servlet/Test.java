6fa0cc5b4f1121b8ea189ba4fa5646d573d6e3be
mvinduwara <mvinduwara@gmail.com>
2026-07-13 23:05:42 +0530

    new commit

---
+++ J2EE-Transactions/src/main/java/com/bcd/servlet/Test.java
@@ -0,0 +1,26 @@
+package com.bcd.servlet;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("JTA-PU");
+
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println(em);
+    }
+}
