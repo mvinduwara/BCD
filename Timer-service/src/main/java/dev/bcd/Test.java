e9fae2c2b3e5610cc80263ad923f73b2ee692c6b
mvinduwara <mvinduwara@gmail.com>
2026-07-27 22:28:28 +0530

    Update Test.java

--- Timer-service/src/main/java/dev/bcd/Test.java
+++ Timer-service/src/main/java/dev/bcd/Test.java
@@ -9,15 +9,27 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(value = "/Test", asyncSupported = true)
+@WebServlet(value = "/Test", asyncSupported = false)
 public class Test extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        super.service(req, resp);
-        resp.getWriter().write("This Is Test Servlet");
+        
+        System.out.println("Start : " + Thread.currentThread().getName());
 
-        AsyncContext asyncContext = req.startAsync();
+        AsyncContext async = request.startAsync();
+        async.start(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
 
+                }
+            }
+            async.complete();
+        });
+        System.out.println("End : " + Thread.currentThread().getName());
     }
 }
