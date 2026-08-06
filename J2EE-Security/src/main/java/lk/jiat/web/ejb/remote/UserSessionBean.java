afde54f1a13dbf3ecb0d29334626e0ec5cf0c1db
mvinduwara <mvinduwara@gmail.com>
2026-08-06 16:42:41 +0530

    Create UserSessionBean.java

---
+++ J2EE-Security/src/main/java/lk/jiat/web/ejb/remote/UserSessionBean.java
@@ -0,0 +1,30 @@
+package lk.jiat.web.ejb.remote;
+
+import jakarta.annotation.security.*;
+import jakarta.ejb.EJB;
+import jakarta.ejb.Stateless;
+
+@Stateless
+@DeclareRoles({"MANAGER"})
+//@RolesAllowed("USER")
+@PermitAll
+public class UserSessionBean {
+
+    @EJB
+    private AdminSessionBean adminSessionBean;
+
+    public void method1(){
+        System.out.println("UserSessionBean: method1");
+        adminSessionBean.doAction();
+    }
+
+    @RolesAllowed({"ADMIN","MANAGER"})
+    public void method2(){
+        System.out.println("UserSessionBean: method2");
+    }
+
+    @DenyAll
+    public void method3(){
+        System.out.println("UserSessionBean: method3");
+    }
+}
