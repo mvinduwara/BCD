7af3cdbdad007b7e7bd0a094d8df45133023aea0
mvinduwara <mvinduwara@gmail.com>
2026-08-17 08:07:19 +0530

    Update ShipmentService.java

--- globaltrade-scm/scm-ejb/src/main/java/com/globaltrade/scm/session/shipment/ShipmentService.java
+++ globaltrade-scm/scm-ejb/src/main/java/com/globaltrade/scm/session/shipment/ShipmentService.java
@@ -1,4 +1,18 @@
 package com.globaltrade.scm.session.shipment;
 
-public class ShipmentService {
+import com.globaltrade.scm.common.dto.ShipmentDTO;
+import com.globaltrade.scm.entity.ShipmentStatus;
+import com.globaltrade.scm.exception.ShipmentNotFoundException;
+import com.globaltrade.scm.exception.VendorNotFoundException;
+
+import jakarta.ejb.Local;
+
+import java.util.List;
+
+@Local
+public interface ShipmentService {
+    List<ShipmentDTO> findAll();
+    ShipmentDTO findById(Long id) throws ShipmentNotFoundException;
+    ShipmentDTO create(ShipmentDTO input) throws VendorNotFoundException;
+    ShipmentDTO updateStatus(Long id, ShipmentStatus status) throws ShipmentNotFoundException;
 }
