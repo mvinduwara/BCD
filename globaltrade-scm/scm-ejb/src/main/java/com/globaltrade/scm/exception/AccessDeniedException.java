8a9e0abf38489e34aa17bc80eef57494968a25fa
mvinduwara <mvinduwara@gmail.com>
2026-08-19 19:37:02 +0530

    Update AccessDeniedException.java

--- globaltrade-scm/scm-ejb/src/main/java/com/globaltrade/scm/exception/AccessDeniedException.java
+++ globaltrade-scm/scm-ejb/src/main/java/com/globaltrade/scm/exception/AccessDeniedException.java
@@ -3,7 +3,7 @@ package com.globaltrade.scm.exception;
 import jakarta.ejb.ApplicationException;
 
 @ApplicationException(rollback = false)
-public class AccessDeniedException extends Exception {
+public class AccessDeniedException extends ScmException {
     public AccessDeniedException(String message) {
         super(message);
     }
